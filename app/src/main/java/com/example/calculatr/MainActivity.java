31cd173f7493ddc58509798f7dca7fd213570ca2
Saumya Darne <darnesaumya@gmail.com>
2019-05-06 15:07:11 +0530

    HEllo2

--- app/src/main/java/com/example/calculatr/MainActivity.java
+++ app/src/main/java/com/example/calculatr/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.calculatr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button one,two,three,four,five,six,seven,eight,nine,zero,add,sub,mult,div,equal;
+    EditText tf1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    MainActivity(){
+        one = findViewById(R.id.button);
+        two = findViewById(R.id.button2);
+        three = findViewById(R.id.button3);
+        four = findViewById(R.id.button4);
+        five = findViewById(R.id.button5);
+        six = findViewById(R.id.button6);
+        seven = findViewById(R.id.button7);
+        eight = findViewById(R.id.button8);
+        nine = findViewById(R.id.button9);
+        zero = findViewById(R.id.button15);
+        add = findViewById(R.id.button10);
+        div = findViewById(R.id.button11);
+        sub = findViewById(R.id.button12);
+        mult = findViewById(R.id.button13);
+    }
 }
